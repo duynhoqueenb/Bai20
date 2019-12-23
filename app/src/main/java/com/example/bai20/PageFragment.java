f2b7fd56c778864c0bd88790ec0269d636ff908c
tranduyqueenb <duytd@queenb.vn>
2019-12-23 16:52:15 +0700

    8

--- app/src/main/java/com/example/bai20/PageFragment.java
+++ app/src/main/java/com/example/bai20/PageFragment.java
@@ -62,7 +62,7 @@ public class PageFragment extends Fragment {
     }
 
     public void createItemListData() {
-        PageFragment1Model datalist = new PageFragment1Model("Cong ty CP VietLive", "Nhan vien thiet ke", "15-18 trieu", "5 nguoi", "30/12/2019", "700", "ung vien (3)");
+        PageFragment1Model datalist = new PageFragment1Model("Cong ty CP VietLive", "Nhan vien thiet ke", "15-18 trieu", "5 nguoi", "30/12/2019", "700", "Ung vien (3)");
         itemList.add(datalist);
 
         datalist = new PageFragment1Model("Cong ty CP VietLive", "Nhan vien thiet ke", "15-18 trieu", "5 nguoi", "30/12/2019", "700", "ung vien (3)");
