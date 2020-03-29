0eeec0bb0baa4122644ffa9a8b74b1729e0a66bd
duynhoqueenb <duytd@queenb.vn>
2020-03-29 21:43:24 +0700

    duy duy

--- app/src/main/java/com/example/bai20/TuyenDung.java
+++ app/src/main/java/com/example/bai20/TuyenDung.java
@@ -7,6 +7,6 @@ public interface TuyenDung {
     //void getTuyenDung(String congviec, String mucluong, String soluong, String thoihan, String diadiem);
 
     void getTuyenDung(PageFragment1Model tuyendungObj, String msg);
-
+    //test
     void getSearch(String msg1);
 }
